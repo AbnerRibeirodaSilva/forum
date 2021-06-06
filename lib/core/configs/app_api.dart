a9694cd656a98b40aa269c19ce82e0c5f9d2d9ef
AbnerRibeiroSilva <sabner838@gmail.com>
2021-06-06 12:07:38 -0300

    Projeto ainda em andamento

---
+++ lib/core/configs/app_api.dart
@@ -0,0 +1,7 @@
+class AppApi {
+  const AppApi._();
+
+  static const appId = '7PuL09BHYp57UYuLlMx77EovoH92yqgm9nyCpVol';
+  static const clientKey = '1WITKbTQz0VzAdt2IERduURNs6PvN0q8d7M3DMav';
+  static const serverUrl = 'https://parseapi.back4app.com/';
+}
